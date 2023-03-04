c4798566bdfa47a358c9c74826400c1481fbc6ea
Amber Jen <amberjen226@gmail.com>
2023-03-04 22:14:36 +0800

    Tip form

--- 01-crypto-tip-jar/main.js
+++ 01-crypto-tip-jar/main.js
@@ -1,7 +1,13 @@
+/*
+* Variables
+*/
 const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
-
 const form = document.querySelector('form');
 
+
+/*
+* Methods
+*/
 const send = async (amount) => {
   
   // Connect the site to MetaMask Wallet first
@@ -14,7 +20,7 @@ const send = async (amount) => {
     - The minimum unit of Ether is called “wei”.
     - 1 ether = 1000000000000000000 wei.
   */ 
-  // Converts any ether value value into wei.
+  // Converts any ether into wei.
   const wei = web3.utils.toWei(amount, 'ether');
 
   // If there's account connected...
@@ -25,13 +31,16 @@ const send = async (amount) => {
         from: accounts[0],
         to: '0xc1aDDD18dE904b6fAfb35811596b916bB1535A90',
         // Convert any given value to HEX
-        // Number strings => numbers
-        // Text strings => UTF-8 strings
         value: web3.utils.toHex(wei)
       }]
     });
   }
 };
+
+
+/*
+* Event Listeners & Inits
+*/
 
 // Only show tip form if user has a wallet
 // MetaMask-specific
@@ -45,7 +54,6 @@ form.addEventListener('submit', (e) => {
   if(window.ethereum) {
     const input = form.querySelector('input');
     send(input.value);
-
   } else {
     alert('Please install a wallet.')
   }

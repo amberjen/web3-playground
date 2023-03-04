e729e159c0094ad28c62d92df2ec092c72ce65dd
Amber Jen <amberjen226@gmail.com>
2023-03-04 21:15:55 +0800

    Project 01 basic html / styles / js setup

---
+++ 01-crypto-tip-jar/main.js
@@ -0,0 +1,53 @@
+const web3 = new Web3(Web3.givenProvider || "ws://localhost:8545");
+
+const form = document.querySelector('form');
+
+const send = async (amount) => {
+  
+  // Connect the site to MetaMask Wallet first
+  const accounts = await window.ethereum.request({
+    method: 'eth_requestAccounts'
+  });
+
+  /*
+    - Ethereum’s currency which is “Ether” has several units.
+    - The minimum unit of Ether is called “wei”.
+    - 1 ether = 1000000000000000000 wei.
+  */ 
+  // Converts any ether value value into wei.
+  const wei = web3.utils.toWei(amount, 'ether');
+
+  // If there's account connected...
+  if(accounts.length > 0) {
+    window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [{
+        from: accounts[0],
+        to: '0xc1aDDD18dE904b6fAfb35811596b916bB1535A90',
+        // Convert any given value to HEX
+        // Number strings => numbers
+        // Text strings => UTF-8 strings
+        value: web3.utils.toHex(wei)
+      }]
+    });
+  }
+};
+
+// Only show tip form if user has a wallet
+// MetaMask-specific
+if(window.ethereum) {
+  form.classList.add('has-eth');
+}
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if(window.ethereum) {
+    const input = form.querySelector('input');
+    send(input.value);
+
+  } else {
+    alert('Please install a wallet.')
+  }
+
+});
